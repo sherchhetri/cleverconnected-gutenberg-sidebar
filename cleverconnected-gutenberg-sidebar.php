7266900a8b124fb7b0f47a8ed94afa2155d03218
sherchhetri <sher.chhetri@gamil.com>
2024-09-09 11:28:18 +0545

    clean  code

--- cleverconnected-gutenberg-sidebar.php
+++ cleverconnected-gutenberg-sidebar.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: cleverconnected gutenberg sidebar
  * Plugin URI:  https://cleverconnected.nl/
@@ -15,14 +16,15 @@
 /**
  * Class cleverconnectedGutenbergSidebar
  */
-class cleverconnectedGutenbergSidebar {
+class cleverconnectedGutenbergSidebar
+{
 
 	const FIELDNAMES = [
 		'_ccgs_hide_in_listing' => 'boolean',
 		'_ccgs_reviewed_by'    => 'string',
 		'_ccgs_translated_by'    => 'string',
-		
-		
+
+
 	];
 
 	/**
@@ -37,8 +39,9 @@ class cleverconnectedGutenbergSidebar {
 	 *
 	 * @return self
 	 */
-	public static function get_instance() {
-		if ( is_null( self::$instance ) ) {
+	public static function get_instance()
+	{
+		if (is_null(self::$instance)) {
 			self::$instance = new self();
 		}
 		return self::$instance;
@@ -47,32 +50,40 @@ class cleverconnectedGutenbergSidebar {
 	/**
 	 * Constructor
 	 */
-	public function __construct() {
-		add_action( 'init', [ $this, 'ccgs_settings_register_meta_fields' ] );
-		add_action( 'enqueue_block_editor_assets', [ $this, 'enqueue_scripts' ] );
-		add_shortcode("cc_reviewed_by",  [ $this,"cc_reviewed_by_shortcode"] );
-		add_shortcode("cc_translated_by",  [ $this,"cc_translated_by_shortcode"] );
+	public function __construct()
+	{
+		add_action('init', [$this, 'ccgs_settings_register_meta_fields']);
+		add_action('enqueue_block_editor_assets', [$this, 'enqueue_scripts']);
+		
+		//short code for reviewed by and translated by
+		add_shortcode("cc_reviewed_by",  [$this, "cc_reviewed_by_shortcode"]);
+		add_shortcode("cc_translated_by",  [$this, "cc_translated_by_shortcode"]);
 	}
 
 
 
-function cc_reviewed_by_shortcode($attrs, $content = null) {
-    global $post;
- 	$reviewed_by  =get_post_meta($post->ID,"_ccgs_reviewed_by",true);
-    return '<div class="ccgs-reviewed-by"> ' .  $reviewed_by  . '</div>';
-}
-function cc_translated_by_shortcode($attrs, $content = null) {
-    global $post;
- 	$translated_by  =get_post_meta($post->ID,"_ccgs_translated_by",true);
-    return '<div class="ccgs-translated-by"> ' .  $translated_by  . '</div>';
-}
+	function cc_reviewed_by_shortcode($attrs, $content = null)
+	{
+		global $post;
+		$reviewed_by  = get_post_meta($post->ID, "_ccgs_reviewed_by", true);
+		return '<div class="ccgs-reviewed-by"> ' .  $reviewed_by  . '</div>';
+	}
+	function cc_translated_by_shortcode($attrs, $content = null)
+	{
+		global $post;
+		$translated_by  = get_post_meta($post->ID, "_ccgs_translated_by", true);
+		return '<div class="ccgs-translated-by"> ' .  $translated_by  . '</div>';
+	}
 
 	/**
 	 * Register fields to the rest API.
 	 */
-	public function ccgs_settings_register_meta_fields() {
-		foreach ( self::FIELDNAMES as $fieldname => $type ) {
-			register_meta( 'post', $fieldname,
+	public function ccgs_settings_register_meta_fields()
+	{
+		foreach (self::FIELDNAMES as $fieldname => $type) {
+			register_meta(
+				'post',
+				$fieldname,
 				[
 					'show_in_rest'  => true,
 					'auth_callback' => fn() => true,
@@ -86,14 +97,15 @@ function cc_translated_by_shortcode($attrs, $content = null) {
 	/**
 	 * Enqueue Scripts
 	 */
-	public function enqueue_scripts() {
-		if ( get_current_screen()->post_type !== 'post' ) {
+	public function enqueue_scripts()
+	{
+		if (get_current_screen()->post_type !== 'post') {
 			return;
 		}
 
 		wp_enqueue_script(
 			'ccgs-settings-sidebar-js',
-			plugin_dir_url( __FILE__ ) . 'build/index.js',
+			plugin_dir_url(__FILE__) . 'build/index.js',
 			[
 				'wp-plugins',
 				'wp-edit-post',
@@ -102,7 +114,7 @@ function cc_translated_by_shortcode($attrs, $content = null) {
 				'wp-data',
 				'wp-dom-ready',
 			],
-			filemtime( dirname( __FILE__ ) . '/build/index.js' )
+			filemtime(dirname(__FILE__) . '/build/index.js')
 		);
 	}
 }
